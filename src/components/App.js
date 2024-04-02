9b5af26b4a33c216a1d8b218694b8f315a43bf1d
Harsh Tyagi <69513043+harshtyagi-code@users.noreply.github.com>
2024-04-02 10:05:30 +0000

    commited

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,30 @@
 import React from "react";
 import './../styles/App.css';
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "../Pages/Home";
-import About from "../Pages/About";
-import Header from "./Header";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 
 const App = () => {
   return (
-    <div>
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
-      </BrowserRouter>
-      {/* Do not remove the main div */}
+    <div id="main">
+        {/* Do not remove the main div */}
+        <BrowserRouter>
+          <ul>
+            <li>
+              <Link to='/'>Home</Link>
+            </li>
+            <li>
+              <Link to='/about'>About</Link>
+            </li>
+          </ul>
+
+          <Routes>
+            <Route path="/" element={<h1 id="home">Welcome to my website!</h1>} />
+            <Route path="/about" element={
+                <p id="about">This is a sample React Router program.</p>
+            } />
+          </Routes>
+        </BrowserRouter>
     </div>
   )
 }
 
-export default App;
+export default App
